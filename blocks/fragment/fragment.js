164dd6711aca6f2e3648df05791c67a04360a0a4
Samir Čauš <samir.caus@gmail.com>
2025-11-17 10:27:36 +0100

    Initial commit

---
+++ blocks/fragment/fragment.js
@@ -0,0 +1,75 @@
+import { loadArea } from '../../scripts/ak.js';
+
+function replaceDotMedia(path, doc) {
+  const resetAttributeBase = (tag, attr) => {
+    doc.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((el) => {
+      el[attr] = new URL(el.getAttribute(attr), new URL(path, window.location)).href;
+    });
+  };
+  resetAttributeBase('img', 'src');
+  resetAttributeBase('source', 'srcset');
+}
+
+/**
+ * Loads a fragment.
+ * @param {string} path The path to the fragment
+ * @returns {HTMLElement} The root element of the fragment
+ */
+export async function loadFragment(path) {
+  const resp = await fetch(`${path}.plain.html`);
+  if (!resp.ok) throw Error(`Couldn't fetch ${path}.plain.html`);
+
+  const html = await resp.text();
+  const doc = new DOMParser().parseFromString(html, 'text/html');
+
+  // Make images cacheable
+  replaceDotMedia(path, doc);
+
+  const sections = doc.body.querySelectorAll(':scope > div');
+  const fragment = document.createElement('div');
+  fragment.classList.add('fragment-content');
+  fragment.append(...sections);
+
+  await loadArea({ area: fragment });
+
+  return fragment;
+}
+
+/**
+ *
+ * @param {Element}} a the fragment link
+ * @returns the element that can be replaced
+ */
+function getReplaceEl(a) {
+  let current = a;
+  const ancestor = a.closest('.section');
+
+  // Walk up the DOM from child to ancestor
+  // Break when there is more than one child
+  while (current && current !== ancestor) {
+    const childCount = current.parentElement.children.length;
+    if (childCount <= 1) {
+      current = current.parentElement;
+    } else {
+      break;
+    }
+  }
+
+  return current;
+}
+
+export default async function init(a) {
+  const path = a.getAttribute('href');
+  const fragment = await loadFragment(path);
+  if (fragment) {
+    const elToReplace = getReplaceEl(a);
+    const sections = fragment.querySelectorAll(':scope > .section');
+    const children = sections.length === 1
+      ? fragment.querySelectorAll(':scope > *')
+      : [fragment];
+    for (const child of children) {
+      elToReplace.insertAdjacentElement('afterend', child);
+    }
+    elToReplace.remove();
+  }
+}
