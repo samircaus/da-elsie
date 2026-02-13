0605973933d93622477d3b8ac273ac95496880ce
samircaus <samir.caus@gmail.com>
2026-02-13 10:45:03 +0100

    update table

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,4 +1,29 @@
+function buildTableFromDivs(tableWrapper) {
+  const rowDivs = [...tableWrapper.querySelectorAll(':scope > div')];
+  if (rowDivs.length === 0) return null;
+
+  const table = document.createElement('table');
+  for (const rowDiv of rowDivs) {
+    const cellDivs = rowDiv.querySelectorAll(':scope > div');
+    const tr = document.createElement('tr');
+    for (const cell of cellDivs) {
+      const td = document.createElement('td');
+      td.innerHTML = cell.innerHTML;
+      tr.append(td);
+    }
+    table.append(tr);
+  }
+  tableWrapper.innerHTML = '';
+  tableWrapper.append(table);
+  return table;
+}
+
 export default function init(el) {
+  const tableWrapper = el.classList.contains('table') ? el : el.querySelector('.table');
+  if (tableWrapper && !tableWrapper.querySelector('table')) {
+    buildTableFromDivs(tableWrapper);
+  }
+
   const tables = el.querySelectorAll('table');
   for (const table of tables) {
     let thead = table.querySelector('table > thead');
